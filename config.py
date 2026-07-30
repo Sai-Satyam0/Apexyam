c2648bda2abd494256795d416fdb193e2d7bfccf
Sai Satyam <shiv190108@gmail.com>
2026-07-30 19:58:19 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+APP_PASSWORD = os.getenv("APP_PASSWORD")
+SMTP_EMAIL = os.getenv("SMTP_EMAIL")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_PHONE_FROM = os.getenv("TWILIO_PHONE_FROM")
+
+DB_PATH = "apexyam.db"
+GROQ_MODEL = "llama-3.3-70b-versatile"
+TTS_VOICE = "en-US-JennyNeural"
+FACE_DATA_DIR = "assets/face_data"
+PERSONALITIES_DIR = "personalities"
+HF_TOKEN = os.getenv("HF_TOKEN")
